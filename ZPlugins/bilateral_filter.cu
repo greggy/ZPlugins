4328e1040ac708e4e5046d743f937d66749ded80
Grigory Fateyev <gfborn@gmail.com>
2013-08-22 15:23:06 +0400

    Bilateral filter.

--- ZPlugins/bilateral_filter.cu
+++ ZPlugins/bilateral_filter.cu
@@ -10,7 +10,8 @@ typedef unsigned char guint8;
 __constant__ float cGaussian[64];   //gaussian array in device side
 texture<uchar4, 2, cudaReadModeNormalizedFloat> rgbaTex;
 
-uint *dImage  = NULL;   //original image
+guint8 *dImage  = NULL;   //original image
+guint8 *dTemp  = NULL;   //temp data
 size_t pitch;
 
 
@@ -75,7 +76,11 @@ bilateral_transform(guint8 *od, int w, int h,
         }
     }
 
-    od[y * w + x] = rgbaFloatToInt2(t/sum);
+    int pixId = (y * w + x) * 4;
+
+    //od[pixId] = rgbaFloatToInt2(pixel);
+    uint tmp = rgbaFloatToInt2(t/sum);
+    memcpy(&od[pixId], &tmp, sizeof(tmp));
 }
 
 
@@ -94,48 +99,78 @@ void updateGaussian(float delta, int radius)
 
 
 void bilateral_transform( guint8 *data, int width, int height ){
-    //guint8 *d_data;
+    guint8 *d_data;
     float gaussian_delta = 4;
     float e_d = 0.1f;
     int radius = 5;
-    //size_t size = width * height * 4;
+    int iterations = 5;
+    size_t size = width * height * 4;
 
     updateGaussian(gaussian_delta, radius);
 
     // copy image data to array
-    checkCudaErrors(cudaMallocPitch(&dImage, &pitch, sizeof(guint8) * width, height));
-    checkCudaErrors(cudaMemcpy2D(dImage, pitch, data, sizeof(guint8) * width,
-                                 sizeof(guint8) * width, height, cudaMemcpyHostToDevice));
-    printf("Pitch: %d\n", pitch);
+    checkCudaErrors(cudaMallocPitch(&dImage, &pitch, sizeof(guint8) * width * 4, height));
+    checkCudaErrors(cudaMallocPitch(&dTemp,  &pitch, sizeof(guint8) * width * 4, height));
+    checkCudaErrors(cudaMalloc(&d_data, size));
+    checkCudaErrors(cudaMemcpy2D(dImage, pitch, data, sizeof(guint8) * width * 4,
+                                 sizeof(guint8) * width * 4, height, cudaMemcpyHostToDevice));
+    //printf("Pitch: %d\n", pitch);
 
     // bind array to texture
     cudaChannelFormatDesc desc = cudaCreateChannelDesc<uchar4>();
     checkCudaErrors(cudaBindTexture2D(0, rgbaTex, dImage, desc, width, height, pitch));
 
-    dim3 gridSize((width + 16 - 1) / 16, (height + 16 - 1) / 16);
-    dim3 blockSize(16, 16);
-    bilateral_transform<<<gridSize, blockSize>>>(data, width, height, e_d, radius);
+    for (int i=0; i<iterations; i++)
+    {
+        dim3 threads = dim3(32, 32, 1);
+        dim3 blocks = dim3(width / threads.x + 1, height / threads.y + 1);
+        bilateral_transform<<<blocks, threads>>>(d_data, width, height, e_d, radius);
+
+        if (iterations > 1)
+        {
+            // copy result back from global memory to array
+            checkCudaErrors(cudaMemcpy2D(dTemp, pitch, d_data, sizeof(guint8) * width * 4,
+                                         sizeof(guint8) * width * 4, height, cudaMemcpyDeviceToDevice));
+            checkCudaErrors(cudaBindTexture2D(0, rgbaTex, dTemp, desc, width, height, pitch));
+        }
+    }
+
+    checkCudaErrors(cudaMemcpy(data, d_data, size, cudaMemcpyDeviceToHost));
 
     checkCudaErrors(cudaFree(dImage));
+    checkCudaErrors(cudaFree(d_data));
+    checkCudaErrors(cudaUnbindTexture(rgbaTex));
+}
+
+/*
+// 1D example without iterations
+void bilateral2_transform( guint8 *data, int width, int height ){
+    guint8 *d_data;
+    float gaussian_delta = 4;
+    float e_d = 0.1f;
+    int radius = 5;
+    size_t size = width * height * 4;
+
+    updateGaussian(gaussian_delta, radius);
+
+    // copy image data to array
+    cudaChannelFormatDesc desc = cudaCreateChannelDesc(8, 8, 8, 8, cudaChannelFormatKindUnsigned);
+    checkCudaErrors(cudaMallocArray(&dImage, &desc, width, height));
+    checkCudaErrors(cudaMalloc(&d_data, size));
+    checkCudaErrors(cudaMemcpyToArray(dImage, 0, 0, data, size, cudaMemcpyHostToDevice));
+
+    // Bind the array to the texture
+    checkCudaErrors(cudaBindTextureToArray(rgbaTex, dImage));
+
+    dim3 gridSize((width + 16 - 1) / 16, (height + 16 - 1) / 16);
+    dim3 blockSize(16, 16);
+    bilateral_transform<<< gridSize, blockSize >>>(d_data, width, height, e_d, radius);
+
+    checkCudaErrors(cudaMemcpy(data, d_data, size, cudaMemcpyDeviceToHost));
+
+    checkCudaErrors(cudaFreeArray(dImage));
+    checkCudaErrors(cudaFree(d_data));
     checkCudaErrors(cudaUnbindTexture(rgbaTex));
 
-//    // copy image data to array
-//    cudaChannelFormatDesc desc = cudaCreateChannelDesc(8, 8, 8, 8, cudaChannelFormatKindUnsigned);
-//    checkCudaErrors(cudaMallocArray(&dImage, &desc, width, height));
-//    checkCudaErrors(cudaMalloc(&d_data, size));
-//    checkCudaErrors(cudaMemcpyToArray(dImage, 0, 0, data, size, cudaMemcpyHostToDevice));
-
-//    // Bind the array to the texture
-//    checkCudaErrors(cudaBindTextureToArray(rgbaTex, dImage));
-
-//    dim3 gridSize((width + 16 - 1) / 16, (height + 16 - 1) / 16);
-//    dim3 blockSize(16, 16);
-//    bilateral_transform<<< gridSize, blockSize >>>(d_data, width, height, e_d, radius);
-
-//    checkCudaErrors(cudaMemcpy(data, d_data, size, cudaMemcpyDeviceToHost));
-
-//    checkCudaErrors(cudaFreeArray(dImage));
-//    checkCudaErrors(cudaFree(d_data));
-//    checkCudaErrors(cudaUnbindTexture(rgbaTex));
-
 }
+*/
